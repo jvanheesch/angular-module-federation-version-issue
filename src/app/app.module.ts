7dc34cb9b2685a23e9d35566a3d08059dad031b9
van Heesch Joris <joris.van.heesch@telenetgroup.be>
2024-02-02 19:19:19 +0100

    import NgxSliderModule @angular-slider/ngx-slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ExampleComponent } from './example/example.component';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ExampleComponent } from './example/example.component';
     ExampleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
