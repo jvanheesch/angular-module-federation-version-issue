a5cdc8359767f21d5318607998e73f283b178c4e
van Heesch Joris <joris.van.heesch@telenetgroup.be>
2024-02-02 23:36:17 +0100

    fix issue

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = withModuleFederationPlugin({
   shared: {
     ...share(
       {
-        "rxjs": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+        "rxjs": {singleton: true, strictVersion: true, requiredVersion: '7.8.1'},
       }
     ),
   },
